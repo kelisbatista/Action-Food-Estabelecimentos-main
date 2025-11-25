3084321d4b36ecbbee111b672d7b3493494e7aba
kelisbatista <keli.2023008264@aluno.iffar.edu.br>
2025-11-25 16:17:56 -0300

    Atualizações locais antes do pull

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { InicioComponent } from './inicio/inicio';
 import { LoginComponent } from './login/login';
 import { CadastroEstabelecimento } from './cadastro/cadastro';
+import { Estabelecimento } from './estabelecimento/estabelecimento';
+
 
 export const routes: Routes = [
 
     { path: '', component: InicioComponent },
-    { path: 'login', component: LoginComponent },
+    { path: 'login', component: Login },
     { path: 'cadastro', component: CadastroEstabelecimento }  
 ];
